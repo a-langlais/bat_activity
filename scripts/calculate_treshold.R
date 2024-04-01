c0860fe36e24ead54b4b6752af7999ee43a829b7
a-langlais <160505900+a-langlais@users.noreply.github.com>
2024-04-01 19:58:18 +0200

    Update calculate_treshold

--- scripts/calculate_treshold.R
+++ scripts/calculate_treshold.R
@@ -1,7 +1,7 @@
 # =======================================================================================
 # Titre: Calcul des métriques de bridage optimales pour un projet éolien
 # Description: Ce script R permet de réaliser une analyse multivariée des des données abiotiques
-#              en fonction des contacts de chauves-souris afin de calculer les mesures de bridage
+#              en fonction des data de chauves-souris afin de calculer les mesures de bridage
 #              les plus adaptées.
 # Auteur: Alexandre LANGLAIS
 # Date: 2024/04/01
@@ -10,7 +10,7 @@
 # Dépendances: ggplot2, lubridate
 # =======================================================================================
 
-calculate_seuils <- function(contacts, meteo, dates = NULL, variables, percent = 95, plot = FALSE) {
+calculate_threshold <- function(data, meteo, dates = NULL, var, percent = 95, plot = FALSE) {
   library(lubridate)
   library(ggplot2)
   
@@ -19,19 +19,19 @@ calculate_seuils <- function(contacts, meteo, dates = NULL, variables, percent =
     stop("L'argument 'dates' doit être un vecteur de longueur 2.")
   }
   
-  if(!is.numeric(pourcentage) || pourcentage < 0 || pourcentage > 100) {
-    stop("L'argument 'pourcentage' doit être un nombre entre 0 et 100.")
+  if(!is.numeric(percent) || percent < 0 || percent > 100) {
+    stop("L'argument 'percent' doit être un nombre entre 0 et 100.")
   }
   
-  if(!all(variables %in% names(meteo))) {
-    stop("Toutes les 'variables' doivent être présentes dans 'meteo'.")
+  if(!all(var %in% names(meteo))) {
+    stop("Toutes les 'var' doivent être présentes dans 'meteo'.")
   }
   
-  if(any(sapply(c(contacts$Date_Time, meteo$Date_Time), function(dt) is.na(dmy_hm(dt)[1])))) {
+  if(any(sapply(c(data$Date_Time, meteo$Date_Time), function(dt) is.na(dmy_hm(dt)[1])))) {
     stop("Une ou plusieurs dates ne sont pas dans le format attendu ou ne peuvent pas être converties.")
   }
   
-  if(nrow(donneesFusionnees) == 0) {
+  if(nrow(data_merged) == 0) {
     stop("La fusion des données a produit un ensemble de données vide. Vérifiez les plages de dates et les formats.")
   }
   
@@ -40,24 +40,24 @@ calculate_seuils <- function(contacts, meteo, dates = NULL, variables, percent =
   }
   
   if(plot) {
-    missing_vars <- !variables %in% names(donneesFusionnees)
+    missing_vars <- !var %in% names(data_merged)
     if(any(missing_vars)) {
-      warning("Les variables suivantes manquent dans les données fusionnées et seront ignorées dans le graphique :", 
-              paste(variables[missing_vars], collapse = ", "))
+      warning("Les var suivantes manquent dans les données fusionnées et seront ignorées dans le graphique :", 
+              paste(var[missing_vars], collapse = ", "))
       plot <- FALSE # Désactiver le plot si les données nécessaires manquent
     }
   }
   
   # Convertir les colonnes de date en POSIXct
-  contacts$Date_Time <- dmy_hm(contacts$Date_Time)
+  data$Date_Time <- dmy_hm(data$Date_Time)
   meteo$Date_Time <- dmy_hm(meteo$Date_Time)
-  contacts$Date_Time <- ceiling_date(contacts$Date_Time, unit = "10 minutes")
+  data$Date_Time <- ceiling_date(data$Date_Time, unit = "10 minutes")
   
   # Filtrer les données si des dates sont fournies
   if (!is.null(dates) && length(dates) == 2) {
     date_start <- dmy(dates[1])
     date_end <- dmy(dates[2])
-    contacts <- contacts[contacts$Date_Time >= date_start & contacts$Date_Time <= date_end, ]
+    data <- data[data$Date_Time >= date_start & data$Date_Time <= date_end, ]
     meteo <- meteo[meteo$Date_Time >= date_start & meteo$Date_Time <= date_end, ]
   }
   
@@ -65,13 +65,13 @@ calculate_seuils <- function(contacts, meteo, dates = NULL, variables, percent =
   meteo <- meteo[hour(meteo$Date_Time) >= 20 | hour(meteo$Date_Time) < 6, ]
   
   # Fusionner les données sur la base de la date et de l'heure
-  data_merged <- merge(contacts, meteo, by = "Date_Time", all = TRUE)
+  data_merged <- merge(data, meteo, by = "Date_Time", all = TRUE)
   
   # Créer la colonne 'is_contacted' pour indiquer la présence ou l'absence de contact
   data_merged$is_contacted <- ifelse(!is.na(data_merged$Id), 1, 0)
   
-  # Calcul des percentiles pour les variables spécifiées
-  percentiles_list <- sapply(variables, function(var) {
+  # Calcul des percentiles pour les var spécifiées
+  percentiles_list <- sapply(var, function(var) {
     # Calcul des quantiles pour le percent spécifié et son inverse
     probs <- c(percent / 100, 1 - (percent / 100))
     quantiles <- quantile(data_merged[[var]], probs = probs, na.rm = TRUE)
@@ -93,7 +93,7 @@ calculate_seuils <- function(contacts, meteo, dates = NULL, variables, percent =
   
   gc()
   # Construction du modèle de régression logistique
-  model <- as.formula(paste("is_contacted ~", paste(variables, collapse = " + ")))
+  model <- as.formula(paste("is_contacted ~", paste(var, collapse = " + ")))
   modele <- glm(model, data = data_merged, family = binomial(link = "logit"))
   gc()
   
@@ -112,7 +112,7 @@ calculate_seuils <- function(contacts, meteo, dates = NULL, variables, percent =
   
   # Générer les graphiques si demandé
   if (plot) {
-    for (var in variables) {
+    for (var in var) {
       p <- ggplot(data_merged, aes_string(x = var, y = "is_contacted")) +
         geom_point(alpha = 0.5) +
         geom_smooth(method = "glm", method.args = list(family = "binomial"), se = FALSE, color = "red") +
